8a77ccefc01dd74c343ce4bd4c941ea1908f4430
Carlos Marco Maza <carlos92marco@gmail.com>
2022-11-21 18:02:31 +0100

    reto 2

---
+++ Java/src/desafios0_9/Reto1.java
@@ -0,0 +1,35 @@
+package desafios0_9;
+/*
+ * Reto #1
+ * ¿ES UN ANAGRAMA?
+ * Fecha publicación enunciado: 03/01/22
+ * Fecha publicación resolución: 10/01/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+ * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * NO hace falta comprobar que ambas palabras existan.
+ * Dos palabras exactamente iguales no son anagrama.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+public class Reto1 {
+	public static void main(String[] args) {
+		String palabra1 = "amor";
+		String palabra2  = "roma";
+		
+		System.out.println(compruebaAnagrama(palabra1, palabra2));
+	}
+
+	public static boolean compruebaAnagrama(String palabra1, String palabra2) {
+		if(palabra1.toLowerCase().equals(palabra2.toLowerCase())) {
+			return false;
+		}
+		return true;
+	}
+}
